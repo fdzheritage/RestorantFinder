cd3902bfd535cd75f5d22ec07026053b9b371b3f
Fernandez del Castil <FernandezDelCastillo.Francisco@cegep-heritage.qc.ca>
2019-05-28 13:13:57 -0400

    Aranged project for Krasimir

--- js/main.js
+++ js/main.js
@@ -1,82 +1,62 @@
+let categorySelect;
+let searchButton;
+let searchBox;
 
-let resultsDiv;
+window.onload = async function () {
+    categorySelect = document.getElementById('categories');
+    searchBox = document.getElementById('searchBox');
+    searchButton = document.getElementById('searchButton');
 
-window.onload = function () {
-    resultsDiv = document.getElementById("results");
-    let searchBox = document.getElementById("search-box");
-    let searchButton = document.getElementById("search-button");
-    searchButton.addEventListener('click', () => {
-        // console.log("Working");
-        let searchTerm = searchBox.value;
-        searchByCity(searchTerm);
-        // console.log(searchTerm);
-    });
-    // searchByCity('chicago');
-    //showAllCategories();
-};
+    // Check if categories are stored in client
+    let categoriesJSON = localStorage.getItem('categories');
+    if (!categoriesJSON) {
+        categoriesJSON = await fetchCategories();
+        localStorage.setItem('categories', JSON.stringify(categoriesJSON));
+    } else {
+        categoriesJSON = JSON.parse(categoriesJSON);
+        //console.log('categories loaded from local storage');
+    }
 
-
-async function searchByCity(city) {
+    // loadCategories(categorySelect, categoriesJSON);
 
 
-    myHeaders = new Headers({
-        'Accept': 'application/json',
-        'user-key': 'dd0bd5a709e94d4b8ebce601ad243771'
-    });
-
-    var myInit = {
-        method: 'GET',
-        headers: myHeaders,
-        mode: 'cors',
-        cache: 'default'
-    };
-
-    var myRequest = new Request(`https://developers.zomato.com/api/v2.1/cities?q=${city}`, myInit);
-
-    const response = await fetch(myRequest);
-    const json = await response.json();
-    let resultHTML = ``;
-    console.log(json);
-    json.location_suggestions.forEach(
-        location => {
-            console.log(location.name);
-            // resultHTML = resultHTML + `<div>${location.name}</div>`;
+    searchButton.addEventListener('click', async () => {
+        let container = document.getElementById("list-of-restaurants");
+        
+        let dataJSON = {
+            name: "Bastardo Tacos",
+            address: "123 rue"
         }
-    );
-    resultsDiv = resultHTML;
-}
 
 
-async function showAllCategories() {
-    myHeaders = new Headers({
-        'Accept': 'application/json',
-        'user-key': 'dd0bd5a709e94d4b8ebce601ad243771'
+        let query = searchBox.value.trim();
+        let locationJSON = await searchLocation(query);
+        loadRestaurantList(container, dataJSON );
     });
-
-    var myInit = {
-        method: 'GET',
-        headers: myHeaders,
-        mode: 'cors',
-        cache: 'default'
-    };
-
-    var myRequest = new Request(`https://developers.zomato.com/api/v2.1/categories`, myInit);
-
-    const response = await fetch(myRequest);
-    const json = await response.json();
-
-
-    // Output to the page as HTML
-
 }
 
 
-function renderAll() {
 
+function loadCategories(element, dataJSON) {
+    categoriesHTML = ``;
+    let { categories } = dataJSON;
+    categories.forEach(category => {
+        let { id, name } = category.categories;
+        categoriesHTML +=
+            `<option value="${id}">${name}</option>`;
+    });
+    element.innerHTML = categoriesHTML;
 }
 
-function renderNav(navId) {
-    let navHTML = `
-    
+
+function loadRestaurantList(container, dataJSON ) {
+    // To be completed by Krasimir
+
+
+    container.innerHTML = `
+        <div>
+            <h2>${dataJSON.name}</h2>
+        </div>
     `;
 }
+
---
+++ js/zomato.js
@@ -0,0 +1,29 @@
+/*   START ZOMATO API SETTINGS */
+let zomato = {};
+zomato.endPoint = 'https://developers.zomato.com/api/v2.1/';
+zomato.init = {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'default',
+    headers: {
+        'Accept': 'application/json',
+        'user-key': 'dd0bd5a709e94d4b8ebce601ad243771'
+    }
+};
+/*   END ZOMATO API SETTINGS */
+
+
+// Return json 
+async function fetchCategories() {
+    let resource = `${zomato.endPoint}/categories`;
+    response = await fetch(resource, zomato.init);
+    json = await response.json();
+    return json;
+}
+
+async function searchLocation(query) {
+    let resource = `${zomato.endPoint}/locations?query=${query}`;
+    response = await fetch(resource, zomato.init);
+    json = await response.json();
+    return json;
+}
